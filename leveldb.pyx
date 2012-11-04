88e8b8d5bfe8cf894ed3e87e7a179a709a00f307
Wouter Bolsterlee <uws@xs4all.nl>
2012-11-04 19:56:34 +0100

    Implement opening a LevelDB database from constructor

--- leveldb.pyx
+++ leveldb.pyx
@@ -6,20 +6,31 @@ cimport leveldb
 __version__ = '%d.%d' % (leveldb.kMajorVersion, leveldb.kMinorVersion)
 
 
+class Error(Exception):
+    pass
+
+
 cdef class LevelDB:
     """LevelDB database
 
     A LevelDB database is a persistent ordered map from keys to values.
     """
 
-    cdef leveldb.DB *db
+    cdef leveldb.DB* db
 
-    def __cinit__(self, str directory):
+    def __cinit__(self, bytes name):
         """Open the underlying database handle
 
-        :param str directory: The database directory
+        :param str name: The name of the database
         """
-        raise NotImplementedError()
+        cdef leveldb.Options options
+        cdef leveldb.Status st
+
+        options.create_if_missing = True
+        st = leveldb.DB_Open(options, name, &self.db)
+
+        if not st.ok():
+            raise Error(st.ToString())
 
     def __dealloc__(self):
         del self.db
