dd5777508993e9c4ee120c078d9e97e9bd9301bf
Wouter Bolsterlee <uws@xs4all.nl>
2012-11-04 17:27:47 +0100

    Add initial test module

---
+++ test_leveldb.py
@@ -0,0 +1,31 @@
+
+from nose.tools import assert_raises
+
+import leveldb
+from leveldb import LevelDB
+
+DB_DIR = 'db/'
+
+
+def test_version():
+    v = leveldb.__version__
+    assert isinstance(v, basestring)
+    assert v.startswith('1.')
+
+
+def test_open():
+    db = LevelDB(DB_DIR)
+    print db
+
+
+def test_close():
+    db = LevelDB(DB_DIR)
+    del db
+
+
+def test_get():
+    db = LevelDB(DB_DIR)
+    print db.get('foo')
+
+    with assert_raises(TypeError):
+        print db.get('foo', 'bar')
