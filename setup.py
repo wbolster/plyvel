847cfba7037398d0424d5cd45523561625416f69
Wouter Bolsterlee <uws@xs4all.nl>
2012-11-04 19:45:57 +0100

    Properly link to leveldb library when building

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 from setuptools.extension import Extension
 
+ext_modules = [
+    Extension("leveldb",
+              ["leveldb.cpp"],
+              libraries=["leveldb"])
+]
+
 setup(name="leveldb-cython",
       version="0.1",
       author="Wouter Bolsterlee",
       author_email="uws@xs4all.nl",
-      ext_modules = [Extension("leveldb", ["leveldb.cpp"])]
+      ext_modules=ext_modules
 )
