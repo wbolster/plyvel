63990c081cb32e7d03d5c57d135eb8288a8a609a
Wouter Bolsterlee <uws@xs4all.nl>
2012-11-09 01:28:28 +0100

    Cosmetic cleanups in setup script

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup
 from setuptools.extension import Extension
 
 ext_modules = [
-    Extension("leveldb",
-              ["leveldb.cpp"],
-              libraries=["leveldb", "snappy"])
+    Extension(
+        'leveldb',
+        ['leveldb.cpp'],
+        libraries=['leveldb', 'snappy'])
 ]
 
-setup(name="leveldb-cython",
-      version="0.1",
-      author="Wouter Bolsterlee",
-      author_email="uws@xs4all.nl",
-      ext_modules=ext_modules
+setup(
+    name='leveldb-cython',
+    version='0.1',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    ext_modules=ext_modules,
 )
