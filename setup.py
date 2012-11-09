b241103f2c95193df222d5d77a25519790aad7ff
Wouter Bolsterlee <uws@xs4all.nl>
2012-11-09 02:11:39 +0100

    Lower package version: just a dev version for now

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ ext_modules = [
 
 setup(
     name='leveldb-cython',
-    version='0.1',
+    version='0.1dev',
     author="Wouter Bolsterlee",
     author_email="uws@xs4all.nl",
     ext_modules=ext_modules,
