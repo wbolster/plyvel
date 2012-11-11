f847d57f7cd6034a083fb8890babd53d0ebb3ac5
Wouter Bolsterlee <uws@xs4all.nl>
2012-11-11 17:31:31 +0100

    Add Sphinx autodoc config flags

--- doc/conf.py
+++ doc/conf.py
@@ -86,6 +86,8 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
+autodoc_default_flags = ['members', 'undoc-members']
+autodoc_member_order = 'bysource'
 
 # -- Options for HTML output ---------------------------------------------------
 
