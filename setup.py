515d8a01689f4d1a110e85e3adcc6622f8fc9c8e
Wouter Bolsterlee <uws@xs4all.nl>
2012-11-15 17:18:46 +0100

    Add some classifiers to the setup script

--- setup.py
+++ setup.py
@@ -15,6 +15,19 @@ setup(
     author_email="uws@xs4all.nl",
     ext_modules=ext_modules,
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: C++",
+        "Programming Language :: Cython",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ]
 )
