c8e28017e9ac76b678231f0765e866fd175e37af
Wouter Bolsterlee <uws@xs4all.nl>
2013-06-03 23:21:33 +0200

    Post-release version bump

--- plyvel/_version.py
+++ plyvel/_version.py
@@ -5,4 +5,4 @@ Plyvel version module.
 # Note: don't add any non-trivial logic here; this file is also loaded
 # from setup.py file when the module has not yet been compiled!
 
-__version__ = '0.3'
+__version__ = '0.4dev'
