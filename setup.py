6cdbdc1776b5c1829b2c4e6466976627e68e85ea
wouter bolsterlee <wouter@bolsterl.ee>
2018-01-03 23:26:41 +0100

    mark as stable in metadata

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     packages=['plyvel'],
     license="BSD License",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
         "Intended Audience :: Science/Research",
