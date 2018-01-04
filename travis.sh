1f4665e907be901cc6e026cd650f528a217a100b
wouter bolsterlee <wouter@bolsterl.ee>
2018-01-04 01:29:08 +0100

    there is no ‘make install’, use alternative on travis

--- travis.sh
+++ travis.sh
@@ -8,4 +8,9 @@ wget https://github.com/google/leveldb/archive/v${LEVELDB_VERSION}.tar.gz
 tar xf v${LEVELDB_VERSION}.tar.gz
 cd leveldb-${LEVELDB_VERSION}/
 make
-sudo make install
+
+# based on https://gist.github.com/dustismo/6203329
+sudo scp -r out-static/lib* out-shared/lib* /usr/local/lib/
+cd include/
+sudo scp -r leveldb /usr/local/include/
+sudo ldconfig
