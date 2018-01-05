5995eda8cc635bd0d10741a001c2c35411a31abe
wouter bolsterlee <wouter@bolsterl.ee>
2018-01-05 22:22:05 +0100

    simplify leveldb installation on travis

--- travis.sh
+++ travis.sh
@@ -8,9 +8,6 @@ wget https://github.com/google/leveldb/archive/v${LEVELDB_VERSION}.tar.gz
 tar xf v${LEVELDB_VERSION}.tar.gz
 cd leveldb-${LEVELDB_VERSION}/
 make
-
-# based on https://gist.github.com/dustismo/6203329
-sudo scp -r out-static/lib* out-shared/lib* /usr/local/lib/
-cd include/
-sudo scp -r leveldb /usr/local/include/
+sudo cp -av out-static/lib* out-shared/lib* /usr/local/lib/
+sudo cp -av include/leveldb/ /usr/local/include/
 sudo ldconfig
