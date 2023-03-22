afe269e81927477bc2b79ec9fe2bfdf843276178
liviaerxin <1yue8haogaoqi@gmail.com>
2023-03-22 22:35:07 +0800

    chore: fix bug

--- scripts/install-snappy.sh
+++ scripts/install-snappy.sh
@@ -41,31 +41,6 @@ cd ~/opt/snappy
 curl -sL https://codeload.github.com/google/snappy/tar.gz/${SNAPPY_VERSION} | tar xzf -
 cd ./snappy-*
 
-
-# Compile snappy
-
-# `CMAKE_INSTALL_NAME_DIR` and `CMAKE_SKIP_INSTALL_RPATH` only have effect for MacOS
-# [CMAKE_SKIP_RPATH/CMAKE_SKIP_INSTALL_RPATH and INSTALL_NAME_DIR precedence on macOS](https://gitlab.kitware.com/cmake/cmake/-/issues/16589)
-# Set `INSTALL_NAME_DIR` to set the install name of shared library to be an absolute path instead of `@rpath/{target_name}`, which will help delocate the `wheel` 
-# Or use `install_name_tool -change <old-path> <new-path> <file>` after .dylib was created
-
-if [[ "$(uname)" == "Darwin" ]]; then
-    INSTALL_NAME_DIR="/usr/local/lib"
-fi
-
-mkdir -p build && cd build
-cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_SKIP_INSTALL_RPATH=OFF \
-    -DCMAKE_INSTALL_NAME_DIR=$INSTALL_NAME_DIR \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-    -DSNAPPY_BUILD_BENCHMARKS=OFF \
-    -DSNAPPY_BUILD_TESTS=OFF \
-    ..
-
-cmake --build . --target install
-
 # Patch inline
 echo $PWD
 patch < $PATCH_FILE
